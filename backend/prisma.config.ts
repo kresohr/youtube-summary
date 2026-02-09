69b91e620a4b8a093304fa26dc28a95659ea710a
KresoHr <ilicic.kresimir@gmail.com>
2026-02-09 22:54:35 +0100

    fix: docker fix

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -4,7 +4,6 @@ export default {
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
     url: process.env.DATABASE_URL,
